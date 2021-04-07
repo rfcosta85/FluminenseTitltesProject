596ff9a46592666a1987460e42d53f6b6dea311d
Rodrigo Costa <rfcosta85@gmail.com>
2021-04-07 17:53:00 -0300

    Finalizando projeto

--- scripts.js
+++ scripts.js
@@ -1,48 +1,68 @@
-function scrollToOnClick(event)
+const brasileiro = document.querySelectorAll('.br-smooth');
+
+brasileiro.forEach(item =>{
+
+    item.addEventListener('click', scrollToIdOnclickBrasileiro);
+
+})
+
+function scrollToIdOnclickBrasileiro(events)
 {
 
-    event.preventDefault();
+    events.preventDefault();
+    const elements = events.target;    
+    const to = document.querySelector("#brasileiro").offsetTop;   
     
-    const to = getScrollTopByHref(event.target); 
-    
-    ScrollToPosition(to);
-
-}
-
-function ScrollToPosition(to)
-{
-
     window.scroll({
 
         top: to,
+
         behavior: "smooth",
 
     });
 
 }
 
-const button_profile = document.querySelectorAll('nav a');
 
-button_profile.forEach(item =>{
+const copaDoBrasil = document.querySelectorAll('.cb-smooth');
+
+copaDoBrasil.forEach(item =>{
+
+    item.addEventListener('click', scrollToIdOnclickCopaDoBrasil);
+
+})
+
+function scrollToIdOnclickCopaDoBrasil(events)
+{
+
+    events.preventDefault();
+    const elements = events.target;    
+    const to = document.querySelector("#cb07").offsetTop;   
+    
+    window.scroll({
+
+        top: to,
+
+        behavior: "smooth",
+
+    });
+
+}
+
+const outros = document.querySelectorAll('.outros-smooth');
+
+outros.forEach(item =>{
 
     item.addEventListener('click', scrollToIdOnclick);
 
 })
-
-function getScrollTopByHref(element)
-{
-
-    const id = element.getAttribute('href');
-    return document.querySelector(id).offsetTop;
-
-}
 
 function scrollToIdOnclick(events)
 {
 
     events.preventDefault();
     const elements = events.target;    
-    const to = document.querySelector("#cb07").offsetTop;   
+    const to = document.querySelector("#outros").offsetTop;   
     
     window.scroll({
 
